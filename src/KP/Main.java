56e3ab175dfc372f2f4586dc12ebf5add79b2998
mojo <moran.brix@gmail.com>
2018-06-27 15:42:31 +0300

    initial

---
+++ src/KP/Main.java
@@ -0,0 +1,14 @@
+package KP;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x=4;
+		int y=8;
+		Point p=new Point(x,y);
+		
+		System.out.println(Integer.toString(x) + "," + Integer.toString(y));
+	}
+
+}
